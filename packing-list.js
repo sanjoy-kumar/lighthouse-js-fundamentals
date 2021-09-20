24ccec12d2ebed9ec911912223e526ff83a8ef4c
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2021-09-20 12:46:37 -0400

    Modiffied packing-list Array Example.

--- packing-list.js
+++ packing-list.js
@@ -1,7 +1,18 @@
+//Iterating Over an Array With for
+
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+
+//Iterating Over an Array With while
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
 
